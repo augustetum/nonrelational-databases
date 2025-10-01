e6da3f225d9ebcf38efcc5743fa4325b6a26d028
zhemepatis <rink.gab@gmail.com>
2025-10-01 18:43:04 +0300

    added bookings repository

---
+++ src/main/java/repository/BookingsRepository.java
@@ -0,0 +1,36 @@
+package repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.bson.Document;
+import org.springframework.stereotype.Repository;
+import config.MongoDbContext;
+import entity.Booking;
+
+@Repository
+public class BookingsRepository {
+    private final MongoDbContext dbContext;
+
+    public BookingsRepository(MongoDbContext dbContext) {
+        this.dbContext = dbContext;
+    }
+
+    public List<Booking> getAllBookings() {
+        return dbContext.bookings.find()
+            .into(new ArrayList<Document>())
+            .stream()
+            .map(this::documentToBooking)
+            .toList();
+    }
+
+    public Booking documentToBooking(Document document) {
+        Booking booking = new Booking();
+
+        booking.setId(document.getString("_id"));
+        booking.setTime(document.getDate("time"));
+        booking.setAddress(document.getString("address"));
+        booking.setDetails(document.getString("details"));
+
+        return booking;
+    }
+}

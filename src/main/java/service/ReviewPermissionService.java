ac36a569f112063a9bb27b8ee55ac5cbe7335487
zhemepatis <rink.gab@gmail.com>
2025-10-11 19:42:03 +0300

    added more validation

--- src/main/java/service/ReviewPermissionService.java
+++ src/main/java/service/ReviewPermissionService.java
@@ -2,17 +2,32 @@ package service;
 
 import java.util.Optional;
 import org.springframework.stereotype.Service;
+
+import dto.ClientDetailsDto;
+import dto.FreelancerDetailsDto;
 import dto.PermissionCheckResultDto;
+import entity.Freelancer;
 import entity.Review;
+import repository.ClientRepository;
 import repository.ClientReviewRepository;
+import repository.FreelancerRepository;
 import repository.FreelancerReviewRepository;
 
 @Service
 public class ReviewPermissionService {
+    private final ClientRepository clientRepository;
+    private final FreelancerRepository freelancerRepository;
     private final ClientReviewRepository clientReviewRepository;
     private final FreelancerReviewRepository freelancerReviewRepository;
 
-    public ReviewPermissionService(ClientReviewRepository clientReviewRepository, FreelancerReviewRepository freelancerReviewRepository) {
+    public ReviewPermissionService(
+        ClientRepository clientRepository, 
+        FreelancerRepository freelancerRepository, 
+        ClientReviewRepository clientReviewRepository, 
+        FreelancerReviewRepository freelancerReviewRepository
+    ) {
+        this.clientRepository = clientRepository;
+        this.freelancerRepository = freelancerRepository;
         this.clientReviewRepository = clientReviewRepository;
         this.freelancerReviewRepository = freelancerReviewRepository;
     }
@@ -20,6 +35,10 @@ public class ReviewPermissionService {
     public PermissionCheckResultDto canAddReview(String revieweeId, String requestorId, boolean isClient) {
         if (revieweeId == requestorId) {
             return PermissionCheckResultDto.invalid("Users are not allowed write reviews to themselves.");
+        }
+
+        if(!userExists(revieweeId, isClient)) {
+            return PermissionCheckResultDto.invalid("User with specified id does not exist.");
         }
 
         Optional<Review> maybeReview; 
@@ -38,6 +57,10 @@ public class ReviewPermissionService {
     }
 
     public PermissionCheckResultDto canEditReview(String revieweeId, String reviewId, String requestorId, boolean isClient) {       
+        if(!userExists(revieweeId, isClient)) {
+            return PermissionCheckResultDto.invalid("User with specified id does not exist.");
+        }
+        
         Optional<Review> maybeReview;
         if(isClient) {
             maybeReview = freelancerReviewRepository.getByReviewId(revieweeId, reviewId);
@@ -59,6 +82,10 @@ public class ReviewPermissionService {
     }
 
     public PermissionCheckResultDto canDeleteReview(String revieweeId, String reviewId, String requestorId, boolean isClient) {
+        if(!userExists(revieweeId, isClient)) {
+            return PermissionCheckResultDto.invalid("User with specified id does not exist.");
+        }
+        
         Optional<Review> maybeReview;
         if(isClient) {
             maybeReview = freelancerReviewRepository.getByReviewId(revieweeId, reviewId);
@@ -77,5 +104,24 @@ public class ReviewPermissionService {
         }
 
         return PermissionCheckResultDto.valid();
+    }
+
+    private boolean userExists(String userId, boolean requestorIsClient) {
+        if (requestorIsClient) {
+            Optional<FreelancerDetailsDto> maybeFreelancer = freelancerRepository.getDetails(userId);
+
+            if (!maybeFreelancer.isPresent()) {
+                return false;
+            }
+        }
+        else {
+            Optional<ClientDetailsDto> maybeClient = clientRepository.getDetails(userId);
+
+            if (!maybeClient.isPresent()) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }

c6ff6c808f4feefba5ac87e283370772eea15530
augustetum <auguste.tumaite@gmail.com>
2025-10-23 10:52:33 +0300

    Add leaderboard functionality, add restassured dependency for later testing

--- src/main/java/controller/FreelancerController.java
+++ src/main/java/controller/FreelancerController.java
@@ -1,5 +1,6 @@
 package controller;
 
+import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +18,16 @@ public class FreelancerController {
     public ResponseEntity<?> getFreelancerDetails(String userId) {
         Optional<FreelancerDetailsDto> maybeFreelancer = freelancerService.getFreelancerDetails(userId);
         return ResponseEntity.ok(maybeFreelancer);
+    }
+
+    @GetMapping("/leaderboard")
+    public ResponseEntity<List<FreelancerDetailsDto>> getLeaderboard(
+        @RequestParam(defaultValue = "averageRating") String sortBy,
+        @RequestParam(defaultValue = "10") int limit,
+        @RequestParam(defaultValue = "0") int skip
+    ) {
+        List<FreelancerDetailsDto> leaderboard = freelancerService.getLeaderboard(sortBy, limit, skip);
+        return ResponseEntity.ok(leaderboard);
     }
 
 }

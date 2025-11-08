e05aa1463f26cbb5cc7a414cb2062533ded388db
augustetum <auguste.tumaite@gmail.com>
2025-11-08 18:59:07 +0200

    Change to Spring websocket, as it is (maybe) better

--- src/main/java/entity/Message.java
+++ src/main/java/entity/Message.java
@@ -1,9 +1,11 @@
 package entity;
-
 import lombok.Data;
+import java.time.LocalDateTime;
 
 @Data
 public class Message {
+    private String id;
+    private LocalDateTime timestamp;
     private String from;
     private String to;
     private String content;

10bf3da6933f2cc27af6c1a1188a35b2af4e1a88
monikaaat <194424400+mtamelyte@users.noreply.github.com>
2025-11-27 16:10:24 +0200

    Add Neo4JConfig and dependencies

---
+++ src/main/java/config/Neo4JConfig.java
@@ -0,0 +1,38 @@
+package config;
+
+import org.neo4j.driver.AuthTokens;
+import org.neo4j.driver.Driver;
+import org.neo4j.driver.GraphDatabase;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import jakarta.annotation.PreDestroy;
+
+public class Neo4JConfig {
+    @Configuration
+    public class Neo4jConfig {
+
+        @Value("${neo4j.uri:bolt://localhost:7687}")
+        private String uri;
+
+        @Value("${neo4j.username:neo4j}")
+        private String username;
+
+        @Value("${neo4j.password:password}")
+        private String password;
+
+        @Bean
+        public Driver neo4jDriver() {
+            return GraphDatabase.driver(uri,
+                    AuthTokens.basic(username, password));
+        }
+
+        @PreDestroy
+        public void closeDriver(@Autowired Driver driver) {
+            driver.close();
+        }
+    }
+
+}

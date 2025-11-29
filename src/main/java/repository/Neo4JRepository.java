f197bf9f3a061ce1ad63e7e83a205a7a53eee349
monikaaat <194424400+mtamelyte@users.noreply.github.com>
2025-11-29 18:34:55 +0200

    Write query for relationship between booking, client and skill

--- src/main/java/repository/Neo4JRepository.java
+++ src/main/java/repository/Neo4JRepository.java
@@ -9,41 +9,45 @@ import org.neo4j.driver.Driver;
 import org.neo4j.driver.Session;
 
 import entity.Booking;
-import entity.Freelancer;
+import entity.Client;
 
 public class Neo4JRepository {
 
     private final Driver driver;
-    private final FreelancerRepository freelancerRepository;
 
-    Neo4JRepository(Driver driver, FreelancerRepository freelancerRepository) {
+    Neo4JRepository(Driver driver) {
         this.driver = driver;
-        this.freelancerRepository = freelancerRepository;
     }
 
     public void addBooking(Booking booking) {
         try (Session session = driver.session()) {
             session.executeWrite(tx -> {
-                Freelancer freelancer = freelancerRepository.findById(booking.getFreelancerId())
-                        .orElseThrow(() -> new RuntimeException("Freelancer not found"));
-
-                String city = freelancer.getCity();
-                if (city == null) {
-                    city = "Nenurodyta";
-                }
 
                 LocalDate date = booking.getTime().toInstant()
                         .atZone(ZoneId.systemDefault())
                         .toLocalDate();
 
                 Map<String, Object> params = new HashMap<>();
+                params.put("skillId", booking.getSkillId());
                 params.put("bookingId", booking.getId());
                 params.put("date", date);
-                params.put("city", city);
-                params.put("client", booking.getClientId());
-                params.put("freelancer", booking.getFreelancerId());
+                params.put("clientId", booking.getClientId());
 
-                tx.run("CREATE (b:Booking {bookingId: $bookingId, date: $date, city: $city, client: $client, freelancer: $freelancer})",
+                tx.run("CREATE (s:Skill {skillId: $skillId})<-[:REQUIRES]-(b:Booking {bookingId: $bookingId, date: $date})<-[:CREATED]-(c:Client {clientId: $clientId})",
+                        params);
+                return null;
+            });
+        }
+    }
+
+    public void addClient(Client client) {
+        try (Session session = driver.session()) {
+            session.executeWrite(tx -> {
+
+                Map<String, Object> params = new HashMap<>();
+                params.put("clientId", client.getId());
+
+                tx.run("CREATE (c:Client {clientId: $clientId})",
                         params);
                 return null;
             });
